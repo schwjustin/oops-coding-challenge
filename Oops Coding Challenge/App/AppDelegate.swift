c1d8e7860a6e0f125c903e26e048ef12046f6732
Justin Schwartz <schw.justin@gmail.com>
2023-04-27 15:34:53 -0400

    remove comments

--- Oops Coding Challenge/App/AppDelegate.swift
+++ Oops Coding Challenge/App/AppDelegate.swift
@@ -11,12 +11,6 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    // Override point for customization after application launch.
-//    let homeViewController = HomeViewController()
-//    let navigationController = UINavigationController(rootViewController: homeViewController)
-//    let window = UIWindow(frame: UIScreen.main.bounds)
-//    window.rootViewController = navigationController
-//    window.makeKeyAndVisible()
     return true
   }
 
